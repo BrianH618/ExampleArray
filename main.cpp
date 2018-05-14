98ce43045aac4111739a9a8c9857b361b4b428cf
brianh618 <bherrera10@mail.csuchico.edu>
2018-05-14 12:27:03 -0700

    final prep

--- main.cpp
+++ main.cpp
@@ -11,19 +11,30 @@ int main()
   string thing;
   string toDo[LIST_SIZE] = {"hello", "second thing", "third thing", "fourth thing"};
  
-  index = 0;
+ /* index = 0;
   while( index < LIST_SIZE )
   {
     cout<<"Give me a thing to do: ";
     cin>>thing;
     toDo[index] = thing;
     index++;
+  }*/
+  for(int index = 0; index < LIST_SIZE; index++)
+  {
+    cout<<"Give me a thing to do: ";
+    cin>>thing;
+    toDo[index] = thing;
   }
-  index = 0;
+  /*index = 0;
   while( index < LIST_SIZE )
   {
     cout<<toDo[index]<<endl;
     index++;
+  }*/
+  for(index = 0; index<LIST_SIZE; index++)
+  {
+    cout<<toDo[index]<<endl;
+
   }
   return 0;
 }
